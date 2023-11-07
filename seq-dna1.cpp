7a9fc2571d53b005a34215fac53ac49d11797b90
Synthia <143859275+Synthia-Li@users.noreply.github.com>
2023-11-07 01:52:57 -0500

    Update seq-dna1.cpp

--- seq-dna1.cpp
+++ seq-dna1.cpp
@@ -1,106 +1,105 @@
 #include <iostream>
 #include <string>
-#include <bitset>
+#include <unordered_map>
 
-// 将字符转换为DNA序列
-std::string charToDNA(char c) {
-    std::bitset<8> ascii(c); // 将字符转换为8位ASCII
+// Function to convert a character to its DNA representation
+std::string charToDNA(char ch) {
+    std::string binary = "";
+    for (int i = 7; i >= 0; --i) {
+        binary += ((ch & (1 << i)) ? "1" : "0");
+    }
+
     std::string dna = "";
-    for (int i = 7; i >= 0; i -= 2) {
-        // 每2位二进制转换为一个DNA碱基
-        switch (ascii[i] * 2 + ascii[i - 1]) {
-            case 0: dna = "A" + dna; break;
-            case 1: dna = "T" + dna; break;
-            case 2: dna = "G" + dna; break;
-            case 3: dna = "C" + dna; break;
+    for (size_t i = 0; i < binary.length(); i += 2) {
+        if (binary.substr(i, 2) == "00") {
+            dna += "A";
+        } else if (binary.substr(i, 2) == "10") {
+            dna += "G";
+        } else if (binary.substr(i, 2) == "01") {
+            dna += "T";
+        } else if (binary.substr(i, 2) == "11") {
+            dna += "C";
         }
     }
     return dna;
 }
 
-// 将DNA序列转换为字符
-char dnaToChar(const std::string& dna) {
-    std::bitset<8> ascii(0);
-    for (size_t i = 0; i < dna.size(); ++i) {
+// Function to convert DNA sequence back to character
+char dnaToChar(const std::string &dna) {
+    int ascii = 0;
+    for (size_t i = 0; i < dna.length(); i++) {
         ascii <<= 2;
         switch (dna[i]) {
-            case 'A': break; // 00不需要改变
-            case 'T': ascii.set(0); break; // 01
-            case 'G': ascii.set(1); break; // 10
-            case 'C': ascii.set(0); ascii.set(1); break; // 11
+            case 'A': break;
+            case 'G': ascii += 2; break;
+            case 'T': ascii += 1; break;
+            case 'C': ascii += 3; break;
         }
     }
-    return static_cast<char>(ascii.to_ulong());
+    return static_cast<char>(ascii);
 }
 
-// 检查是否是有效的DNA序列
-bool isValidDNA(const std::string& dna) {
-    for (char c : dna) {
-        if (c != 'A' && c != 'G' && c != 'T' && c != 'C') {
+// Function to check if the DNA is valid
+bool isValidDNA(const std::string &dna) {
+    if (dna.length() % 4 != 0) return false;
+    for (char nucleotide : dna) {
+        if (nucleotide != 'A' && nucleotide != 'G' && nucleotide != 'T' && nucleotide != 'C') {
             return false;
         }
     }
     return true;
 }
 
-// 将字符串转换为DNA序列
-std::string stringToDNA(const std::string& text) {
+// Function to convert string to DNA sequence
+std::string textToDNA(const std::string &text) {
     std::string dna = "";
-    for (char c : text) {
-        dna += charToDNA(c);
+    for (char ch : text) {
+        dna += charToDNA(ch);
     }
     return dna;
 }
 
-// 将DNA序列转换为字符串
-std::string dnaToString(const std::string& dna) {
+// Function to convert DNA sequence to string
+std::string dnaToText(const std::string &dna) {
     std::string text = "";
-    for (size_t i = 0; i < dna.size(); i += 4) {
+    for (size_t i = 0; i < dna.length(); i += 4) {
         text += dnaToChar(dna.substr(i, 4));
     }
     return text;
 }
 
 int main() {
-    std::string input, output;
-    char choice;
-    std::cout << "Welcome to the Text-DNA Converter!" << std::endl;
+    std::string choice, sequence;
+    std::cout << "Welcome to the DNA Sequence Converter!" << std::endl;
 
-    do {
-        std::cout << "\nChoose an option:\n"
-                  << "1. Convert sequences to DNA\n"
-                  << "2. Convert DNA to sequences\n"
-                  << "Q. Quit\n"
-                  << "Your choice: ";
+    while (true) {
+        std::cout << "Enter '1' to convert text to DNA, '2' to convert DNA to text, or 'Q' to quit: ";
         std::cin >> choice;
-        if (choice == 'Q' || choice == 'q') {
+
+        if (choice == "Q") {
             break;
+        } else if (choice != "1" && choice != "2") {
+            std::cout << "Invalid choice. Please enter '1' or '2' or 'Q'." << std::endl;
+            continue;
         }
 
-        std::cout << "Enter the sequence: ";
-        std::cin.ignore(); // 忽略之前读取choice后留下的换行符
-        std::getline(std::cin, input); // 使用getline来允许空格
+        std::cout << "Enter the sequence you want to convert: ";
+        std::cin.ignore(); // ignore any leftover characters in the input stream
+        std::getline(std::cin, sequence);
 
-        switch (choice) {
-            case '1':
-                output = stringToDNA(input);
-                std::cout << "DNA Sequence: " << output << std::endl;
-                break;
-            case '2':
-                if (isValidDNA(input)) {
-                    output = dnaToString(input);
-                    std::cout << "Converted Sequence: " << output << std::endl;
-                } else {
-                    std::cout << "Invalid DNA sequence. Please enter a sequence containing only A, G, T, and C." << std::endl;
-                    continue;
-                }
-                break;
-            default:
-                std::cout << "Invalid choice, please try again." << std::endl;
+        if (choice == "1") {
+            std::string dnaSequence = textToDNA(sequence);
+            std::cout << "The DNA sequence is: " << dnaSequence << std::endl;
+        } else {
+            if (!isValidDNA(sequence)) {
+                std::cout << "Invalid DNA sequence. Please ensure it contains only 'A', 'G', 'T', 'C' and is a multiple of 4 in length." << std::endl;
                 continue;
+            }
+            std::string textSequence = dnaToText(sequence);
+            std::cout << "The text sequence is: " << textSequence << std::endl;
         }
-    } while (true);
+    }
 
-    std::cout << "Goodbye!" << std::endl;
+    std::cout << "Thank you for using the DNA Sequence Converter!" << std::endl;
     return 0;
 }
