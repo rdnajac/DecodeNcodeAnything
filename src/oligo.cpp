6a61f6e665b3d34c9d6419bd4773c493edf4a01b
Synthia-li <li.xinjin@columbia.edu>
2023-12-06 15:28:26 -0500

    fix issue 2

--- src/oligo.cpp
+++ src/oligo.cpp
@@ -21,6 +21,7 @@ private:
     /**
      * @brief Data block storing the binary representation of the oligonucleotide.
      */
+    
     uint64_t data_block;
 
 public:
@@ -78,14 +79,31 @@ public:
      * @param other The other oligo to compare.
      * @return -1 if the oligo is less than the other, 0 if equal, and 1 if greater.
      */
-    int cmp(const Oligo& other) const {
+   /* int cmp(const Oligo& other) const {
         int bpComparison = std::minmax(bp(), other.bp()).first;
         if (bpComparison != 0) return bpComparison;
 
         int dataComparison = std::minmax(data_block, other.data()).first;
         return dataComparison;
-    }
+    }*/
+    int cmp(const Oligo& other) const {
+        // Compare lengths of sequences
+        if (bp() != other.bp()) {
+            return (bp() < other.bp()) ? -1 : 1;
+        }
 
+        // Compare sequences character by character
+        for (size_t i = 0; i < bp(); ++i) {
+            int nt1 = (*this)[i];
+            int nt2 = other[i];
+
+            if (nt1 != nt2) {
+                return (nt1 < nt2) ? -1 : 1;
+            }
+        }
+
+        return 0;  // Sequences are identical
+    }
     bool operator==(const Oligo& other) const { return cmp(other) == 0; }
     bool operator!=(const Oligo& other) const { return cmp(other) != 0; }
     bool operator<(const Oligo& other) const { return cmp(other) < 0; }
@@ -108,7 +126,7 @@ public:
      * @param end The ending index of the slice.
      * @return A new Oligo object or Oligo() if the slice is invalid.
      */
-    Oligo slice(size_t start, size_t end) const {
+ /*   Oligo slice(size_t start, size_t end) const {
         end = (end <= 0) ? bp() - end : std::min(end, bp());
         end = (end < 0) ? 0 : end;
         start = (start > bp() || start > end) ? 0 : start;
@@ -117,8 +135,23 @@ public:
         uint64_t omask = (1ULL << (2 * new_bp)) - 1;
 
         return Oligo(new_bp, (data_block >> (2 * (bp() - end))) & omask);
-    }
+    }*/
 
+    Oligo slice(size_t start, size_t end) const {
+        // Handle negative indices
+        if (start < 0) start = 0;
+        if (end < 0) end = 0;
+
+        // Adjust indices within valid range
+        if (start >= bp()) start = bp() - 1;
+        if (end >= bp()) end = bp() - 1;
+        if (end < start) end = start;
+
+        auto new_bp = end - start + 1;
+        uint64_t omask = (1ULL << (2 * new_bp)) - 1;
+
+        return Oligo(new_bp, (data_block >> (2 * (bp() - end - 1))) & omask);
+    }
     /**
      * @brief Append another oligo to the current oligo.
      * @param other The other oligo to append.
